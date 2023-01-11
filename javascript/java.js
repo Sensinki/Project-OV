7b2a305052da12ae50e107eb054eabc0813e5eb2
Sensinki <107122047+Sensinki@users.noreply.github.com>
2023-01-11 17:54:41 +0100

    Add files via upload

---
+++ javascript/java.js
@@ -0,0 +1,15 @@
+
+	function openNav() {
+		document.getElementById("myNav").style.width = "100%";
+	}
+
+	function closeNav() {
+		document.getElementById("myNav").style.width = "0%";
+	}
+
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+ 
